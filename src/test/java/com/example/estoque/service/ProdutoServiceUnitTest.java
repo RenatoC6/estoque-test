a571c38d5b5a1238012b7975d7613ca404070404
campi <campionr6@gmail.com>
2025-05-05 22:09:31 -0300

    criação dos primeiros casos de testes unitarios

--- src/test/java/com/example/estoque/service/ProdutoServiceUnitTest.java
+++ src/test/java/com/example/estoque/service/ProdutoServiceUnitTest.java
@@ -1,11 +1,41 @@
 package com.example.estoque.service;
 
+import com.example.estoque.repository.ProdutoRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 public class ProdutoServiceUnitTest {
 
+    private ProdutoRepository repository;
+    private ProdutoService produtoService;
+
+    @BeforeEach
+    public void setup() {
+        System.out.println("Executando o before each");
+
+        repository = Mockito.mock(ProdutoRepository.class);
+
+        produtoService = new ProdutoService(repository);
+    }
+
+    // Dado, quando e então
+
+    // teste1: Tentar cadastrar um produto que não existe no banco de dados.
     @Test
-    public void primeiroTeste(){
+    public void dadoUmProduto_quandoEleNaoExiste_tentarCadastrar(){
+
+    }
+
+    // teste2: Setar a quantidade do pedido em um pedido existente.
+    @Test
+    public void dadoUmProduto_quandoEleExiste_setarQuantidade(){
+
+    }
+
+    // teste3: Testar a falha na comunicação com o BD (repository) ao criar produto.
+    @Test
+    public void dadoUmProduto_quandoEleNaoExiste_gravarNoRepository(){
 
     }
 }
