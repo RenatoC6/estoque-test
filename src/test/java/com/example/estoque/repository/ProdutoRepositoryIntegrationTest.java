c320f0224d837a493b046dfa118db8058cde86a3
campi <campionr6@gmail.com>
2025-05-07 21:43:25 -0300

    teste de integração

--- src/test/java/com/example/estoque/repository/ProdutoRepositoryIntegrationTest.java
+++ src/test/java/com/example/estoque/repository/ProdutoRepositoryIntegrationTest.java
@@ -1,6 +1,7 @@
 package com.example.estoque.repository;
 
 import com.example.estoque.domain.Produto;
+import com.example.estoque.entity.ProdutoEntity;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,21 +10,21 @@ import java.util.List;
 
 public class ProdutoRepositoryIntegrationTest {
 
-
     @Autowired
     private ProdutoRepository repository;
 
     @Test
     public void dadoProdutoExiste_quandoFindByNome_entaoAchouProduto() {
         //var nome = RandomStringUtils.randomNumeric(15);
-        var produto = new Produto();
+        var produto = new ProdutoEntity();
         produto.setNome("Jose");
         produto.setDescricao("xxxxxx");
         produto.setPreco(10.0);
         produto.setQtd(5);
         repository.save(produto);
 
-        var found = repository.findByNome(produto);
-
+        var found = repository.findByNome("jose");
         Assertions.assertNotNull(found);
+
+    }
 }
