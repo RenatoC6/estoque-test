8370d3053b66b889228824f3eef0de737b6aefc0
Leandro <lstoccobr@gmail.com>
2025-05-13 21:04:59 -0300

    adicionados os testes

--- src/test/java/com/example/estoque/controller/EstoqueControllerComponentTest.java
+++ src/test/java/com/example/estoque/controller/EstoqueControllerComponentTest.java
@@ -1,15 +1,19 @@
 package com.example.estoque.controller;
 
+import com.example.estoque.domain.ItemPedido;
 import com.example.estoque.domain.Pedido;
 import com.example.estoque.domain.Produto;
 import com.example.estoque.entity.ProdutoEntity;
+import com.example.estoque.exception.ForaDeEstoqueException;
 import com.example.estoque.service.ProdutoService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -17,8 +21,11 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.Collections;
+import java.util.List;
 
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doThrow;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -96,9 +103,81 @@ public class EstoqueControllerComponentTest {
     }
 
 
+    //------------------------
+
+    @Autowired
+    private EstoqueController controller;
 
 
+    //Teste de pedido vazio
+
+    @Test
+    void deveRetornarOkParaPedidoVazio() {
+        Pedido pedido = new Pedido();
+        pedido.setItens(List.of());
+
+        ResponseEntity<String> response = controller.atualizarEstoque(pedido);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
     }
+
+    //Teste de atualização de estoque
+
+    @Test
+    void deveAtualizarEstoqueComSucesso() {
+        Pedido pedido = new Pedido();
+        pedido.setItens(List.of(new ItemPedido()));
+
+        doNothing().when(service).atualizarEstoque(pedido);
+
+        ResponseEntity<String> response = controller.atualizarEstoque(pedido);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("Estoque Atualizado", response.getBody());
+    }
+
+    //Teste falha por falta de estoque
+    @Test
+    void deveFalharQuandoEstoqueInsuficiente() {
+        Pedido pedido = new Pedido();
+        ItemPedido item = new ItemPedido();
+        item.setId(1L);
+        item.setQtd(100);
+        pedido.setItens(List.of(item));
+
+        doThrow(new ForaDeEstoqueException("Produto fora de estoque"))
+                .when(service).atualizarEstoque(pedido);
+
+        ResponseEntity<String> response = controller.atualizarEstoque(pedido);
+
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals("Produto fora de estoque", response.getBody());
+    }
+
+    //TESTE COM PRODUTO INEXISTENTE
+    @Test
+    void deveFalharQuandoProdutoNaoExiste() {
+        Pedido pedido = new Pedido();
+        ItemPedido item = new ItemPedido();
+        item.setId(1L);
+        item.setQtd(1);
+        pedido.setItens(List.of(item));
+
+        doThrow(new ForaDeEstoqueException("Produto não encontrado"))
+                .when(service).atualizarEstoque(pedido);
+
+        ResponseEntity<String> response = controller.atualizarEstoque(pedido);
+
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals("Produto não encontrado", response.getBody());
+    }
+
+
+}
+
+
+
+
 
 
 
